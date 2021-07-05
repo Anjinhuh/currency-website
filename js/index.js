d9ef9414f53eee6a228b517706fdb56dcaa6afbf
Anjinhuh <juliano.alfredo86@gmail.com>
2021-07-05 11:39:44 -0300

    Updates, but not completed, need more big updates :=)

--- js/index.js
+++ js/index.js
@@ -25,69 +25,84 @@
     const ok = new Date()
     const dataAtual = String(date.getUTCFullYear())+'0'+String(date.getUTCMonth()+1)+ date.getUTCDate()
     const dataAntiga = String(dataAtual-7)
-window.onload = function (){
-    if(dataAntiga < 0){
-        dataAtual = 31
-    }
-    fetch(`https://economia.awesomeapi.com.br/USD-BRL/7?start_data=${dataAntiga}&end_date=${dataAtual}`).then(x =>{
-        return x.json()
-    }).then(precoDolar =>{
-        const dolar = precoDolar[0]
-        const date  =  String(dolar.create_date[8]  +  dolar.create_date[9])
 
-
-                var dataPoints = [];
-                
-                var options =  {
-                    animationEnabled: true,
-                    theme: "light1",
-                    title: {
-                        text: "Cotação"
-                    },
-                    axisX: {
-                        valueFormatString: dataPoints.x,
-                        interval:1,
-                    },
-                    axisY: {
-                        title: "R$",
-                        titleFontSize: 30,
-                        valueFormatString: dataPoints.y,
-                        interval: 0.01,
-                     },
-                    data: [{
-                        type: "spline", 
-                        yValueFormatString: dataPoints.y,
-                        dataPoints: dataPoints
-                    }]
-                };
-                
-                function addData(data) {
-                    if(data){
-                        for (var i = 0; i < 7; i++) {
-                            if(date > 0){
-                                let date = 31
-                                dataPoints.push({
-                                    x: Number(date-i),
-                                    y: Number(data[i].ask)
-                               });
-                            } else{
-                                dataPoints.push({
-                                    x: Number(date-i),
-                                    y: Number(data[i].ask)
-                               });
+        
+        window.onload = function (){
+            
+            fetch(`https://economia.awesomeapi.com.br/USD-BRL/7?start_data=${dataAntiga}&end_date=${dataAtual}`).then(x =>{
+                return x.json()
+            }).then(precoDolar =>{
+                const dolar = precoDolar[0]
+                const date  =  String(dolar.create_date[8]  +  dolar.create_date[9])
+        
+        
+                        var dataPoints = [];
+                        
+                        var options =  {
+                            animationEnabled: true,
+                            theme: "light1",
+                            title: {
+                                text: "Cotação"
+                            },
+                            axisX: {
+                                valueFormatString: dataPoints.x,
+                                interval:1,
+                            },
+                            axisY: {
+                                title: "R$",
+                                titleFontSize: 30,
+                                valueFormatString: dataPoints.y,
+                                interval: 0.01,
+                             },
+                            data: [{
+                                type: "spline", 
+                                yValueFormatString: dataPoints.y,
+                                dataPoints: dataPoints
+                            }]
+                        };
+                        
+                        function addData(data) {
+                            var buyCurrency = document.getElementById('currency-buy-price')
+                            var sellCurrency = document.getElementById('currency-sell-price')
+                            var sellCurrency2 = document.getElementById('currency-sell-price2')
+                            var percentualVariation = document.getElementById('variation-percentual')
+                            
+                            if(data){
+                                 buyCurrency.innerHTML = precoDolar[0].bid
+                                 sellCurrency.innerHTML = precoDolar[0].ask
+                                 sellCurrency2.innerHTML = precoDolar[0].ask                         
+                                 percentualVariation.innerHTML = precoDolar[0].varBid +'%'
+                          
+                                for (var i = 0; i < 7; i++) {
+                                    if(date > 0){
+                                        let date = 7
+                                        dataPoints.push({
+                                            x: Number(date-i),
+                                            y: Number(data[i].ask)
+                                       });
+                                    } else{
+                                        dataPoints.push({
+                                            x: Number(date-i),
+                                            y: Number(data[i].ask)
+                                       });
+                                    }
+                                     
+                                    
+                                }
+                                
                             }
-                             
-                            console.log(dataPoints)
+                            
+                            $("#chartContainer").CanvasJSChart(options);
+                            
                         }
-                    }
-                    
-                    $("#chartContainer").CanvasJSChart(options);
-                
-                }
-                $.getJSON(`https://economia.awesomeapi.com.br/USD-BRL/7?start_data=${dataAntiga}&end_date=${dataAtual}`, addData);
-                
-                
-                
-                
-        })
-    }
+                        $.getJSON(`https://economia.awesomeapi.com.br/USD-BRL/7?start_data=${dataAntiga}&end_date=${dataAtual}`, addData);
+                        
+                        
+                        
+                        
+                })
+            }
+async function currency(currencyValue){
+    currency.moeda = "USD-BRL"
+   
+ }
